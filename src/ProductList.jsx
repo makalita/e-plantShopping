cb27f09c6a4be05b73a168f95bc927fe1068f2e5
Mateo Adrian Kalafatovich <148184139+makalita@users.noreply.github.com>
2025-07-22 17:04:28 -0400

    Fix add button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -310,12 +310,12 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-title">{plant.name}</div> {/* Display plant name */}
                                 {/* Display other plant details like description and cost */}
                                 <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                                <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                                <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                 <button
-                                    className="product-button"
+                                    className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`}
                                     onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                                 >
-                                    Add to Cart
+                                    {addedToCart[plant.name] ? 'Added' : 'Add to Cart'}
                                 </button>
                                 </div>
                             ))}
